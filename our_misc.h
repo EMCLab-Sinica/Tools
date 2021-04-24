c8ff843b889e398c58f50c77dbdc6a9bbd801219
Chih-Hsuan Yen <yan12125@gmail.com>
2021-04-24 20:24:02 +0800

    Implement our_delay_cycles() on MSP430
    Built-in __delay_cycles() only accepts constants, and thus not useful
    for cases like waiting for the first value in DMA+LEA pipelines.

--- our_misc.h
+++ our_misc.h
@@ -6,8 +6,8 @@
 extern "C" {
 #endif
 
-inline void our_delay_cycles_internal(uint32_t n_cycles) {
 #ifdef __arm__
+inline void our_delay_cycles_internal(uint32_t n_cycles) {
     // Based on an example in https://community.arm.com/developer/ip-products/processors/b/processors-ip-blog/posts/condition-codes-1-condition-flags-and-codes
     // %= guarantees unique labels. See https://www.keil.com/support/man/docs/armclang_ref/armclang_ref_wan1517569524985.htm
     __asm__ volatile(".Lour_delay_cycles_loop%=:\n"
@@ -15,8 +15,12 @@ inline void our_delay_cycles_internal(uint32_t n_cycles) {
                      "bne .Lour_delay_cycles_loop%=\n"
                      :
                      : [n_cycles] "r"(n_cycles));
-#endif
 }
+#elif defined(__MSP430__)
+void our_delay_cycles_internal(uint16_t n_cycles);
+#elif defined(__linux__)
+#define our_delay_cycles_internal(n_cycles)
+#endif
 
 // subs takes 1 cycle, bne takes 1 + P cycles, and MSP432 uses a 3-stage pipeline
 #define our_delay_cycles(n_cycles) our_delay_cycles_internal(n_cycles / 3)
